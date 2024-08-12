5599d66711684bb56c7c5a053a3aee0bbd7332bb
Aarjav Jain <anshjain638@gmail.com>
2024-08-12 18:24:30 +0530

    added custom hooks Named useFetch

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import  Quote  from '../constants/types';
+
+const useFetch = (url: string): [Quote[] | null, boolean] => {
+    const [data, setData] = useState<Quote[] | null>(null);
+    const [loading, setLoading] = useState<boolean>(true);
+
+    const fetchData = async () => {
+        const response = await fetch(url);
+        const result = await response.json();
+        setData(result);
+        setLoading(false);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, [url]);
+
+    return [data, loading];
+};
+
+export default useFetch;
