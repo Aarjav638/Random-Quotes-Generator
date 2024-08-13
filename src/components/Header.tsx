360df3a463bbc25282ec3936b9ad9993f4ac07cb
Aarjav Jain <anshjain638@gmail.com>
2024-08-13 15:57:01 +0530

    Updated link address in header and added a signIn option

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,53 @@
 import React, { useState } from 'react'
-
+import SignInModal from './auth/Modal'
+import { Link } from 'react-router-dom'
 const Header: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false)
-  console.log(isOpen)
+  const [isModalOPen, setIsModalOpen] = useState<boolean>(false)
+
+  const handleClose = () => {
+    setIsModalOpen(false)
+  }
+
+
+
+
   return (
     <>
       <header className='flex justify-between flex-row bg-teal-500 min-w-full text-white py-4 px-4 md:px-8 items-center'>
-        <a href='#'> <h1 className='text-xl md:text-2xl font-semibold mr-6 md:ml-6 text-center'>Random Quotes Generator</h1></a>
+        <Link to='/'> <h1 className='text-xl md:text-2xl font-semibold mr-6 md:ml-6 text-center'>Random Quotes Generator</h1></Link>
 
         <img src="/menu.png" alt="logo" className='flex md:hidden w-6 h-6 md:w-16 md:h-16' onClick={() => { setIsOpen(!isOpen) }} />
         <div className='w-[25%] hidden md:flex'>
           <ul className='flex-row flex gap-x-6 '>
-            <li><a href="#" referrerPolicy="no-referrer" >Home</a></li>
-            <li><a href="#" referrerPolicy='no-referrer'>About</a></li>
-            <li><a href="#" referrerPolicy='no-referrer'>Contact</a></li>
+            <li><Link to="/" referrerPolicy="no-referrer" >Home</Link></li>
+            <li><Link to="/about" referrerPolicy='no-referrer'>About</Link></li>
+            <li><Link to="/contact" referrerPolicy='no-referrer'>Contact</Link></li>
           </ul>
         </div>
+
+        <span className='hidden md:flex'> <button className='bg-white text-teal-500 px-4 py-2 rounded-lg' onClick={() => {
+          setIsModalOpen(true)
+        }}>Sign in</button></span>
 
 
       </header>
       {isOpen && <div className="w-full bg-teal-500">
         <ul className='flex flex-col px-4 text-white gap-y-2 items-center '>
-          <a href='#'>Home</a>
-          <a href='#'>About</a>
-          <a href='#'>Contact</a>
+          <li><Link to="/" referrerPolicy="no-referrer" >Home</Link></li>
+          <li><Link to="/about" referrerPolicy='no-referrer'>About</Link></li>
+          <li><Link to="/contact" referrerPolicy='no-referrer'>Contact</Link></li>
+          <button className='bg-white text-teal-500 px-4 py-2 rounded-full mb-2' onClick={() => {
+            setIsModalOpen(true)
+          }} >Sign in</button>
         </ul>
 
       </div>}
+      {
+        isModalOPen &&
+        <SignInModal isOpen={isModalOPen} handleClose={handleClose} />
+
+      }
     </>
   )
 }
