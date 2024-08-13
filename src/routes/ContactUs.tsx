3e244897be0709dbc73870fb6d53bae4949a2919
Aarjav Jain <anshjain638@gmail.com>
2024-08-13 18:03:46 +0530

    Added Zod Schema for Validation

--- src/routes/ContactUs.tsx
+++ src/routes/ContactUs.tsx
@@ -1,98 +1,130 @@
-import React, { useRef } from 'react'
-import { useForm } from 'react-hook-form'
-import Header from '../components/Header'
-import Footer from '../components/Footer'
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import { formValues } from "../Schema/zodSchema";
+import { contactUsSchema } from "../Schema/zodSchema";
 
-type formValues = {
-    name: string
-    email: string
-    phone: string
-    message: string
-}
+const ContactUs: React.FC = () => {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<formValues>({
+        resolver: zodResolver(contactUsSchema),
+    });
 
-const ContactUs = () => {
-    const nameRef = useRef<HTMLInputElement>(null)
-    const emailRef = useRef<HTMLInputElement>(null)
-    const messageRef = useRef<HTMLTextAreaElement>(null)
-    const phoneRef = useRef<HTMLInputElement>(null)
-    const submitRef = useRef<HTMLButtonElement>(null)
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault()
-        const form: formValues = {
-
-            name: nameRef.current!.value,
-            email: emailRef.current!.value,
-            message: messageRef.current!.value,
-            phone: phoneRef.current!.value
-        }
-        console.log(form)
-    }
-    console.log(messageRef)
-    const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>, nextRef: React.RefObject<HTMLInputElement>
-        | React.RefObject<HTMLButtonElement> | React.RefObject<HTMLTextAreaElement>
-    ) => {
-        if (e.key === 'Enter') {
-            e.preventDefault();
-            nextRef.current?.focus();
-        }
+    const onSubmit: SubmitHandler<formValues> = (data) => {
+        console.log(data);
     };
+
     return (
         <>
             <Header />
             <div className="flex flex-col justify-center items-center">
-                <h1 className='my-4 text-4xl'>ContactUs</h1>
-                <div className='flex mb-4 bg-slate-500 w-4/5 md:w-3/4 lg:w-1/2  rounded-lg justify-center items-center p-4'>
-                    <form className="flex flex-col  mb-4 w-full justify-center items-center" onSubmit={handleSubmit} >
-                        <div className='flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4'>
-                            <label htmlFor="name" className='text-white'>Name:</label>
-                            <input
-                                type='text'
-                                placeholder='Name'
-                                className='p-2 w-full sm:w-2/3 border border-gray-300 rounded-md'
-                                required={true}
-                                ref={nameRef}
-                                onKeyDown={(e) => handleKeyDown(e, emailRef)}
-                            />
-                        </div>
-                        <div className='flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4'>
-                            <label htmlFor="email" className='text-white'>Email:</label>
-                            <input
-                                type="email"
-                                placeholder="Email"
-                                className="p-2 w-full sm:w-2/3 border border-gray-300 rounded-md"
-                                required={true}
-                                ref={emailRef}
-                                onKeyDown={(e) => handleKeyDown(e, phoneRef)}
-                            />
+                <h1 className="my-4 text-4xl">ContactUs</h1>
+                <div className="flex mb-4 bg-slate-500 w-4/5 md:w-3/4 lg:w-1/2  rounded-lg justify-center items-center p-4">
+                    <form
+                        className="flex flex-col  mb-4 w-full justify-center items-center"
+                        onSubmit={handleSubmit(onSubmit)}
+                    >
+                        {/* Name Field */}
+                        <div className="flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4">
+                            <label htmlFor="name" className="text-white">
+                                Name:
+                            </label>
+                            <div className="w-full sm:w-2/3 gap-y-2 justify-center flex flex-col">
+                                <input
+                                    {...register("name")}
+                                    type="text"
+                                    placeholder="Name"
+                                    className="p-2 w-full  border border-gray-300 rounded-md"
+                                    aria-invalid={errors.name ? "true" : "false"}
+                                />
+                                {errors.name && (
+                                    <p role="alert" className="text-teal-400 bg-black">
+                                        {errors.name.message}
+                                    </p>
+                                )}
+                            </div>
                         </div>
 
-                        <div className='flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4'>
+                        {/* Email Field */}
+                        <div className="flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4">
+                            <label htmlFor="email" className="text-white">
+                                Email:
+                            </label>
+                            <div className="w-full sm:w-2/3 gap-y-2 justify-center  flex flex-col">
+                                <input
+                                    {...register("email")}
+                                    type="email"
+                                    placeholder="Email"
+                                    className="p-2 w-full  border border-gray-300 rounded-md"
+                                    aria-invalid={errors.email ? "true" : "false"}
+                                />
+                                {errors.email && (
+                                    <p role="alert" className="text-teal-400 bg-black">
+                                        {errors.email.message}
+                                    </p>
+                                )}
+                            </div>
+                        </div>
 
-                            <label htmlFor="phone" className='text-white'>Phone:</label>
-                            <input
-                                type="tel"
-                                placeholder="Phone"
-                                className="p-2 border w-full sm:w-2/3  border-gray-300 rounded-md"
-                                required={true}
-                                ref={phoneRef}
-                                onKeyDown={(e) => handleKeyDown(e, messageRef)}
-                            />
+                        {/* Phone Field */}
+                        <div className="flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4">
+                            <label htmlFor="phone" className="text-white">
+                                Phone:
+                            </label>
+                            <div className="w-full sm:w-2/3 gap-y-2 justify-center  flex flex-col">
+                                <input
+                                    {...register("phone")}
+                                    type="tel"
+                                    placeholder="Phone"
+                                    className="p-2 w-full border border-gray-300 rounded-md"
+                                    aria-invalid={errors.phone ? "true" : "false"}
+                                    maxLength={10}
+                                    onInput={(e: React.ChangeEvent<HTMLInputElement>) => {
+                                        e.target.value = e.target.value.slice(0, 10);
+                                    }}
+                                />
+                                {errors.phone && (
+                                    <p role="alert" className="text-teal-400 bg-black">
+                                        {errors.phone.message}
+                                    </p>
+                                )}
+                            </div>
                         </div>
-                        <div className='flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4'>
-                            <label htmlFor="message" className='text-white'>Message:</label>
-                            <textarea
-                                className='p-2 w-full sm:w-2/3 min-h-48 border border-gray-300 rounded-md'
-                                required={true}
-                                ref={messageRef}
-                            />
+
+                        {/* Message Field */}
+                        <div className="flex flex-col sm:flex-row gap-4 items-center justify-between w-full  sm:w-3/4 p-4">
+                            <label htmlFor="message" className="text-white">
+                                Message:
+                            </label>
+                            <div className="w-full sm:w-2/3 gap-y-2 justify-center flex flex-col">
+                                <textarea
+                                    {...register("message")}
+                                    className="p-2 w-full  min-h-48 border border-gray-300 rounded-md"
+                                    aria-invalid={errors.message ? "true" : "false"}
+                                />
+                                {errors.message && (
+                                    <p role="alert" className="text-teal-400 bg-black">
+                                        {errors.message.message}
+                                    </p>
+                                )}
+                            </div>
                         </div>
-                        <button className="bg-teal-500 text-white p-2 rounded-md" type='submit' ref={submitRef} >Sign In</button>
+
+                        {/* Submit Button */}
+                        <button className="bg-teal-500 text-white p-2 rounded-md" type="submit">
+                            Submit
+                        </button>
                     </form>
                 </div>
             </div>
             <Footer />
         </>
-    )
-}
+    );
+};
 
 export default ContactUs;
